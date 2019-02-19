23b5875f59b43c118558b09edb8079f202ff69ba
dummy <dummy>
2019-02-19 12:57:31 +0100

    Add project files.

---
+++ CastleWindsorAspNetCoreFacilityRepro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CastleWindsorAspNetCoreFacilityRepro.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
